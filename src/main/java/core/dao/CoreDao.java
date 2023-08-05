6fb95c2893f7b1d052febd78a82d08413aa71222
Jimmy <a0916803159@gmail.com>
2023-08-05 14:02:28 +0800

    change ch12

--- src/main/java/core/dao/CoreDao.java
+++ src/main/java/core/dao/CoreDao.java
@@ -2,14 +2,22 @@ package core.dao;
 
 import java.util.List;
 
+import org.hibernate.Session;
+
+import static core.util.HibernateUtil.getSessionFactory;;
+
 public interface CoreDao<P, I> {
-	int insert(P pojo);
+    int insert(P pojo);
 
-	int deleteById(I id);
+    int deleteById(I id);
 
-	int update(P pojo);
+    int update(P pojo);
 
-	P selectById(I id);
+    P selectById(I id);
 
-	List<P> selectAll();
+    List<P> selectAll();
+
+    default Session getSession(){
+        return getSessionFactory().getCurrentSession();
+    }
 }
